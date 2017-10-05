7c29bbd9e4d11b0bbb80b0c5d37a471bc464e74b
Alexander Chu <alchu@mymail.mines.edu>
2017-10-04 18:40:45 -0600

    Added calcAdjacencies

--- src/expirament/IntBoard.java
+++ src/expirament/IntBoard.java
@@ -47,7 +47,24 @@ public class IntBoard {
 	 * Calculates adjacencies
 	 */
 	public void calcAdjacencies() {
-
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				Set<BoardCell> Cells = new HashSet<BoardCell>(); 
+				if (i - 1 >= 0) {
+					Cells.add(grid[i - 1][j]);
+				}
+				if (j - 1 >= 0) {
+					Cells.add(grid[i][j - 1]);
+				}
+				if (i + 1 < rows) {
+					Cells.add(grid[i + 1][j]);
+				}
+				if (j + 1 < columns) {
+					Cells.add(grid[i][j + 1]);
+				}
+				adjCells.put(grid[i][j], Cells);
+			}
+		}
 	}
 	
 	public void calcTargets(BoardCell startCell, int pathLength) {
