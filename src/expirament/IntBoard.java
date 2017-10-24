c531edf0902fb1e594ee0ca754ac7711288af346
Alexander Chu <alchu@mymail.mines.edu>
2017-10-23 19:39:59 -0600

    Starting calcAdjacencies

--- src/expirament/IntBoard.java
+++ src/expirament/IntBoard.java
@@ -1,11 +1,12 @@
 package expirament;
 
-import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import clueGame.BoardCell;
 
 public class IntBoard {
 	private BoardCell[][] grid;
@@ -88,37 +89,7 @@ public class IntBoard {
 	public BoardCell getCell(int row, int column) {
 		return grid[row][column];
 	}
-	public void print() {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++) {
-				System.out.print(grid[i][j].toString() + " ");
-				
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void main(String[] args) {
-		IntBoard board = new IntBoard(4,4);
-		board.calcAdjacencies();
-		BoardCell cell = board.getCell(2, 2);
-		
-		board.calcTargets(cell, 2);
-		Set<BoardCell> targets = board.getTargets();
-		System.out.println();
-		for (BoardCell a : targets) {
-			System.out.println(a.toString());
-		}
-		BoardCell cell2 = board.getCell(2, 1);
-		board.calcTargets(cell2, 2);
-		Set<BoardCell> targets2 = board.getTargets();
-		System.out.println();
-		for (BoardCell b : targets2) {
-			System.out.println(b.toString());
-		}
-	}
-	
-	
+
 
 	
 	
--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -7,7 +7,7 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 
-import expirament.BoardCell;
+import clueGame.BoardCell;
 import expirament.IntBoard;
 
 public class IntBoardTests {
