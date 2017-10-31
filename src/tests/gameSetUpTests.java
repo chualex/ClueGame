50b67a00bc5e9b24823a5ec7faa56be4e07de781
Alexander Chu <alchu@mymail.mines.edu>
2017-10-31 14:50:46 -0600

    Start gameSetUpTests

---
+++ src/tests/gameSetUpTests.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
+public class gameSetUpTests {
+	
+	public static final int LEGEND_SIZE = 11;
+	public static final int NUM_ROWS = 23;
+	public static final int NUM_COLUMNS = 25;
+	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueGameBoard.csv", "ClueLegend.txt");		
+		// Initialize will load BOTH config files 
+		board.initialize();
+	}
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
