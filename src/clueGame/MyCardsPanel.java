cad6faaee2f9a4f1090baea0e399f92e07c2ee14
Joseph O'Brien <jobrien1@mymail.mines.edu>
2017-11-20 20:24:51 -0700

    Added player's cards to the myCards panel

--- src/clueGame/MyCardsPanel.java
+++ src/clueGame/MyCardsPanel.java
@@ -16,7 +16,13 @@ public class MyCardsPanel extends JPanel{
 	private JTextField weapons;
 	private JTextField rooms;
 	
+	private static Board board;
+	
 	public MyCardsPanel() {
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueGameBoard.csv", "ClueLegend.txt", "PlayerFile.txt", "WeaponsFile.txt");
+		board.initialize();
 	//Creates layout for the board panel
 	setLayout(new GridLayout(3,0));
 	JPanel panel = createPeoplePanel();
@@ -29,10 +35,20 @@ public class MyCardsPanel extends JPanel{
 	}
 	
 	private JPanel createWeaponsPanel() {
+		boolean mult = false;
+		String text  = "";
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1,1));
 		JLabel label = new JLabel("Weapons:");
 		weapons = new JTextField(20);
+		int i = 0;
+		for (Card card: board.getPlayers()[0].getMyCards()) {
+			if (card.getCardType() == CardType.WEAPON) {
+				text += card.getCardName() + "---";
+			}
+			
+		}
+		weapons.setText(text);
 		weapons.setEditable(false);
 		panel.add(label);
 		panel.add(weapons);
@@ -40,10 +56,18 @@ public class MyCardsPanel extends JPanel{
 	}
 	
 	private JPanel createPeoplePanel() {
+		String text  = "";
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1,1));
 		JLabel label = new JLabel("People:");
 		people = new JTextField(20);
+		for (Card card: board.getPlayers()[0].getMyCards()) {
+			if (card.getCardType() == CardType.PERSON) {
+				text += card.getCardName() + "---";
+			}
+			
+		}
+		people.setText(text);
 		people.setEditable(false);
 		panel.add(label);
 		panel.add(people);
@@ -52,10 +76,18 @@ public class MyCardsPanel extends JPanel{
 	
 	
 	private JPanel createRoomsPanel() {
+		String text = "";
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1,1));
 		JLabel label = new JLabel("Rooms:");
 		rooms = new JTextField(20);
+		for (Card card: board.getPlayers()[0].getMyCards()) {
+			if (card.getCardType() == CardType.ROOM) {
+				text += card.getCardName() + "---";
+			}
+			
+		}
+		rooms.setText(text);
 		rooms.setEditable(false);
 		panel.add(label);
 		panel.add(rooms);
